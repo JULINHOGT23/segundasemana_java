a6e17f58d2ddaee991447e6cd92151ce8f43cfbe
JULINHOGT23 <1485840@senati.pe>
2023-03-18 10:58:22 -0500

    sama con java

---
+++ JavaApplication37/src/NewClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NewClass {
+    
+public static void main(String[] args) {
+Scanner leer = new Scanner(System.in);
+System.out.println("ingrese el numero final para la suma : ");
+int = n1 leer.nextlnt();
+
+
+   
+}
+
+
+
+}
+
